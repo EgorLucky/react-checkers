708b398547ccadb0fa64031b18e6c6b0173c72ee
EgorLucky <luk_9694@hotmail.com>
2022-10-10 16:34:18 +0300

    update JoinGame: add gameInfo fetching

--- src/components/JoinGame/index.tsx
+++ src/components/JoinGame/index.tsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { GameGetInfoResult, GameState } from "../../serviceApi/models/models";
+import ServiceApi from "../../serviceApi/serviceApi";
+
 function JoinGame() {
+    const [gameInfo, setGameInfo] = useState<GameGetInfoResult|null>(null);
+    //const [registerButtonDisabled, setReg]
+    const { id } = useParams();
+      if(id === undefined){
+        alert("wrong url");
+        return;
+      }
+
+    useEffect(() => {
+      const getGameInfo = async () => {
+        if(gameInfo == null){
+          const newGameInfo = await ServiceApi.GetInfo(id);
+          setGameInfo(newGameInfo);
+        }
+      }
+
+      getGameInfo().catch(console.log);
+    })
+
+    const registerClick = async () => {
+      
+    }
+
     return (
         <div>
-          JoinGame
+          { gameInfo == null && <>Getting info about game...</> }
+          { gameInfo?.state === GameState.Created && <><button onClick={registerClick}>Register in game</button></> }
         </div>
     );
   }
